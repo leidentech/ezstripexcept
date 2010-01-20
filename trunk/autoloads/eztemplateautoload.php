e303c914784413255349b1fc84abfc1395f64142
Steven.E.Bailey <Steven.E.Bailey@99763e34-05e2-11df-956f-01f849d29158>
2010-01-20 16:46:08 +0000

    Initial Project Import

---
+++ trunk/autoloads/eztemplateautoload.php
@@ -0,0 +1,8 @@
+<?php
+
+$eZTemplateOperatorArray = array();
+$eZTemplateOperatorArray[] =
+	array( 'script' => 'extension/strip_except/stripexceptoperator.php',
+                            'class' => 'StripexceptOperator',
+                            'operator_names' => array( 'strip_except' ) );
+?>
